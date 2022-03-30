bc03ded84d4238e4db9ce03a8293f986eabc3270
Brian Jackson <bjack205@gmail.com>
2022-03-30 19:49:09 -0400

    Update solutions

--- src/airplane.jl
+++ src/airplane.jl
@@ -21,13 +21,17 @@ function set_mesh!(vis, ::YakPlane; color=nothing, scale=0.15)
     settransform!(vis["robot"]["geom"], compose(Translation(0,0,0.07),LinearMap( RotY(pi/2)*RotZ(-pi/2) * scale)))
 end
 
-function RobotDynamics.discrete_dynamics(model::YakPlane, x, u, t, dt)
-    RobotDynamics.discrete_dynamics(RK4, model, x, u, t, dt)
+function dynamics(model::YakPlane, x, u)
+    RobotDynamics.dynamics(model, SVector{13}(x), SVector{4}(u))
+end
+
+function discrete_dynamics(model::YakPlane, x, u, t, dt)
+    RobotDynamics.discrete_dynamics(RobotDynamics.RK4, model, SVector{13}(x), SVector{4}(u), t, dt)
 end
 
 function discrete_jacobian(model::YakPlane, x, u, t, dt)
     ∇f = RobotDynamics.DynamicsJacobian(model)
-    z = KnotPoint(SVector{13}(x), SVector{4}(u), dt, t)
-    discrete_jacobian!(RK4, ∇f, model, z)
+    z = RobotDynamics.StaticKnotPoint(SVector{13}(x), SVector{4}(u), dt, t)
+    RobotDynamics.discrete_jacobian!(RobotDynamics.RK4, ∇f, model, z)
     return RobotDynamics.get_static_A(∇f), RobotDynamics.get_static_B(∇f)
 end
